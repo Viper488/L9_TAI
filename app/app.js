723df46ee162420a17c17945ad024dc1d13f60a7
Artur Hamernik <hamernik18@gmail.com>
2021-05-17 08:53:40 +0200

    zad4 and 5 - business, DAO, endpoints, connection to database

--- app/app.js
+++ app/app.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
 import routes from "./REST/routes";
+import mongoose from 'mongoose';
 
 const app = express();
 app.use(express.static(__dirname + '/public'));
@@ -15,6 +16,26 @@ app.use(bodyParser.json({limit: '2048kb'}));
 app.use(express.static('public'));
 
 app.use(cors());
+
+mongoose.connect(config.databaseUrl, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}, (error) => {
+  if (error) {
+    console.error(error);
+  }
+  else {
+    console.info('Connect with database established');
+  }
+});
+
+process.on('SIGINT', () => {
+  mongoose.connection.close(function () {
+    console.error('Mongoose default connection disconnected through app termination');
+    process.exit(0);
+  });
+});
 
 routes(app);
 
